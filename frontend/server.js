1de3b6dd0cf99316f49da9ff821b51bc29d940ac
a-mhamdi <a_mhamdi@outloook.com>
2026-02-01 15:32:08 +0100

    frontend update in nodejs

--- frontend/server.js
+++ frontend/server.js
@@ -1,15 +1,32 @@
-const express = require('express');
-const path = require('path');
+require("dotenv").config();
+
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')), express.static(path.join(__dirname, 'js')));
+app.use(
+  express.static(path.join(__dirname, "public")),
+  express.static(path.join(__dirname, "js")),
+);
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`MWE listening on port ${port}`);
+const API_URL = process.env.BACKEND_URL;
+console.log(API_URL);
+
+app.get("/config.js", (req, res) => {
+  res.type("application/javascript");
+  res.send(`
+    window.CONFIG = {
+      API_URL: "${API_URL}"
+    };
+  `);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`MWE listening on port ${PORT}`);
 });
